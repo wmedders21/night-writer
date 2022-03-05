8c8be4b46c021837a5e4282def00511800862bc0
Will Medders <wmedders@gmail.com>
2022-03-05 13:38:00 -0500

    Feat: find character register

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,12 @@
+require 'pry'
 class Translator
   attr_reader :text, :dictionary
   def initialize(text, dictionary)
     @text = text
     @dictionary = dictionary
+  end
+
+  def find_register(character)
+    @dictionary.character_hash.map { |k,v| k if v.any?(character) }.compact.first
   end
 end
