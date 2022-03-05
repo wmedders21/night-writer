02ea7443133ceadedcb8e57261ba1aa9ef6fe6d0
Will Medders <wmedders@gmail.com>
2022-03-05 09:12:08 -0500

    Spec: Translator exists

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -2,8 +2,17 @@ require 'pry'
 require 'rspec'
 require 'simplecov'
 require './lib/translator'
+require './lib/dictionary'
 
 SimpleCov.start
 
-# RSpec.describe Translator do
-# end
+RSpec.describe Translator do
+  before :each do
+    @braille = Dictionary.new
+    @translator = Translator.new("a", @braille)
+  end
+
+  it 'exists' do
+    expect(@translator).to be_a(Translator)
+  end
+end
