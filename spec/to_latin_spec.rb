385f3790d540086cfecbdfb18672e54fa18ccaba
Will Medders <wmedders@gmail.com>
2022-03-06 17:27:00 -0500

    Spec: #collect_rows

--- spec/to_latin_spec.rb
+++ spec/to_latin_spec.rb
@@ -24,4 +24,9 @@ RSpec.describe ToLatin do
     expect(@to_latin.text).to eq("0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n")
     expect(@to_latin.dictionary).to eq(@latin)
   end
+
+  it 'collect_rows' do
+    expect(@to_latin.collect_rows).to be_a(Array)
+    expect(@to_latin.collect_rows.count).to be(3)
+  end
 end
