1f2592cad4111ba172e464793e24c62eec440755
Will Medders <wmedders@gmail.com>
2022-03-06 17:22:42 -0500

    Spec: has attributes

--- spec/to_latin_spec.rb
+++ spec/to_latin_spec.rb
@@ -18,4 +18,10 @@ RSpec.describe ToLatin do
   it 'exists' do
     expect(@to_latin).to be_a(ToLatin)
   end
+
+  it 'has attributes' do
+    expect(@to_latin.text).to be_a(String)
+    expect(@to_latin.text).to eq("0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n")
+    expect(@to_latin.dictionary).to eq(@latin)
+  end
 end
