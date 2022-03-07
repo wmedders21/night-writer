c52d3c5d0af79bfbf2999d12cd6d47b7146ab510
Will Medders <wmedders@gmail.com>
2022-03-07 14:50:23 -0500

    Refactor: delete Readable, Writeable and associated specs

--- lib/readable.rb
+++
@@ -1,9 +0,0 @@
-module Readable
-  def read
-    opener = File.open(ARGV[0], 'r')
-    incoming_text = opener.read.downcase
-    puts "Created '#{ARGV[1]}' containing #{incoming_text.length} characters"
-    opener.close
-    incoming_text
-  end
-end
--- lib/writeable.rb
+++
@@ -1,7 +0,0 @@
-module Writeable
-  def write(input)
-    writer = File.open(ARGV[1], 'w')
-    writer.write(input)
-    writer.close
-  end
-end
