8a640e3c63040e2317eca48dadef021885b75c33
Will Medders <wmedders@gmail.com>
2022-03-07 12:01:24 -0500

    Spec: IO exists

--- spec/io_spec.rb
+++ spec/io_spec.rb
@@ -5,10 +5,16 @@ require './lib/io'
 SimpleCov.start
 
 RSpec.describe IO do
-
-  it 'read' do
+  before :each do
     ARGV.replace(['sample1.txt', 'braille.txt'])
     io = IO.new(ARGV)
+  end
+
+  it 'exists' do
+    expect(io).to be_a(IO)
+  end
+
+  xit 'read' do
     expect(io.read).to be_a(String)
     expect(io.read.length).to be(308)
   end
