24eff2378ccbb4400ace8059622272e8d344e7ee
Will Medders <wmedders@gmail.com>
2022-03-07 12:43:24 -0500

    Spec: #make_new_file

--- spec/io_spec.rb
+++ spec/io_spec.rb
@@ -5,24 +5,37 @@ require './lib/io'
 SimpleCov.start
 
 RSpec.describe IO do
-  before :each do
-    @io = IO.new(ARGV)
-    ARGV.replace(['sample1.txt', 'braille.txt'])
+  context 'reading' do
+    before :each do
+      @io = IO.new(ARGV)
+      ARGV.replace(['sample1.txt', 'braille.txt'])
+    end
+
+    it 'exists' do
+      expect(@io).to be_a(IO)
+    end
+
+    it 'get_text' do
+      expect(@io.text).to eq("")
+      @io.get_text
+      expect(@io.text).to be_a(String)
+      expect(@io.text.length).to be(308)
+    end
+
+    it 'make_statement' do
+      @io.get_text
+      expect(@io.make_statement).to eq("Created 'braille.txt' containing 308 characters")
+    end
   end
 
-  it 'exists' do
-    expect(@io).to be_a(IO)
-  end
+  context 'writing' do
 
-  it 'get_text' do
-    expect(@io.text).to eq("")
-    @io.get_text
-    expect(@io.text).to be_a(String)
-    expect(@io.text.length).to be(308)
-  end
-
-  it 'make_statement' do
-    @io.get_text
-    expect(@io.make_statement).to eq("Created 'braille.txt' containing 308 characters")
+    it 'make_new_file()' do
+      make_new_file('apple')
+      file = File.open('spec.txt', 'r')
+      test_text = file.read
+      file.close
+      expect(test_text).to eq('apple')
+    end
   end
 end
