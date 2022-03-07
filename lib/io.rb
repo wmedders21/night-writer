d720545ffa247d47d4341607596d46c403f0069d
Will Medders <wmedders@gmail.com>
2022-03-07 12:23:31 -0500

    Feat: #get_text

--- lib/io.rb
+++ lib/io.rb
@@ -1,6 +1,15 @@
 class IO
-
   def initialize(arg_array)
     @arg_array = arg_array
   end
+
+  def get_text
+    opener = File.open(@arg_array[0], 'r')
+    incoming_text = opener.read.downcase
+    opener.close
+    incoming_text
+  end
 end
+
+
+# puts "Created '#{ARGV[1]}' containing #{incoming_text.length} characters"
