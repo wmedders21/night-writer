3bbb2b98938ad0f7eb58aa46152306820265b77a
Will Medders <wmedders@gmail.com>
2022-03-07 14:47:37 -0500

    Feat: night_reader works with IO

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -3,7 +3,9 @@ require './lib/to_latin'
 require './lib/latin'
 require './lib/io'
 
-# latin = Latin.new
-# translated = ToLatin.new(read, latin)
-# rendered_text = translated.formatted
-# write(rendered_text)
+latin = Latin.new
+io = IO.new(ARGV)
+translated = ToLatin.new(io.get_text, latin)
+rendered_text = translated.formatted
+puts io.make_statement
+io.make_new_file(rendered_text)
--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -6,8 +6,5 @@ require './lib/io'
 braille = Dictionary.new
 io = IO.new(ARGV)
 rendered_text = Translator.translate(io.get_text, braille)
-io.make_statement
-io.make_new_file(rendered_text)
-# braille = Dictionary.new
-# rendered_text = Translator.translate(read, braille)
-# write(rendered_text)
+puts io.make_statement
+io.make_new_file(rendered_text.formatted)
