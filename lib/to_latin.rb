c96793a15c05f98170c45511cc04503990ed5470
Will Medders <wmedders@gmail.com>
2022-03-06 19:00:57 -0500

    Feat: #parse_rows

--- lib/to_latin.rb
+++ lib/to_latin.rb
@@ -7,4 +7,21 @@ class ToLatin
     @text = text
     @dictionary = dictionary
   end
+
+  def collect_rows
+    @text.split("\n")
+  end
+
+  def parse_rows
+    all_rows = collect_rows
+    until all_rows == []
+      rows = all_rows[0..2]
+      array = []
+      (rows[0].length / 2).times do
+        array << rows.map { |row| row.slice!(0..1) }.join
+      end
+      all_rows.delete("")
+    end
+    array
+  end
 end
