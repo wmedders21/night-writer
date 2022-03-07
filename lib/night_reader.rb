48037cb8806172664e5a964e52a57824b31aaec0
Will Medders <wmedders@gmail.com>
2022-03-07 14:38:12 -0500

    Feat: refactor night_writer to use IO

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,13 +1,9 @@
 require 'pry'
 require './lib/to_latin'
 require './lib/latin'
-require './lib/readable'
-require './lib/writeable'
+require './lib/io'
 
-include Readable
-include Writeable
-
-latin = Latin.new
-translated = ToLatin.new(read, latin)
-rendered_text = translated.formatted
-write(rendered_text)
+# latin = Latin.new
+# translated = ToLatin.new(read, latin)
+# rendered_text = translated.formatted
+# write(rendered_text)
--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,12 +1,13 @@
 require 'pry'
 require './lib/translator'
 require './lib/dictionary'
-require './lib/readable'
-require './lib/writeable'
-
-include Readable
-include Writeable
+require './lib/io'
 
 braille = Dictionary.new
-rendered_text = Translator.translate(read, braille)
-write(rendered_text)
+io = IO.new(ARGV)
+rendered_text = Translator.translate(io.get_text, braille)
+io.make_statement
+io.make_new_file(rendered_text)
+# braille = Dictionary.new
+# rendered_text = Translator.translate(read, braille)
+# write(rendered_text)
